6063ec02bd58a370f7541fc3a7a40364a28975f6
LW <LW1337@lw.com>
2025-01-09 10:48:36 +0500

    1

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from utils.json_handler import get_items, get_my_ip
 
 if __name__ == "__main__":
     print(get_items())
+    with open('1.txt', 'rw') as file:
+        file.write(get_items())
     get_my_ip()
